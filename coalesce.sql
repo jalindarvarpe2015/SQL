215ad6889cce950b4c38369ff4555b98a165b073
Varpe Jalindar <jalindarvarpe2015@gmail.com>
2024-07-17 16:52:02 +0530

    adding new chamges

--- coalesce.sql
+++ coalesce.sql
@@ -1,4 +1,9 @@
--- The COALESCE function in SQL is used to return the first non-NULL value from a list of expressions. 
---It is often used to handle NULL values in queries, providing a way to substitute them with a specified default value.
+/* 
+
+The COALESCE function in SQL is used to return the first non-NULL value from a list of expressions. 
+It is often used to handle NULL values in queries, providing a way to substitute them with a specified default value.
+
+*/
 
 
+--COALESCE can take multiple arguments, and it evaluates each in order until it finds a non-NULL value.
