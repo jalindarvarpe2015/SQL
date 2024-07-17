aa45be762db9d526a203179733344d5f73e25847
Varpe Jalindar <jalindarvarpe2015@gmail.com>
2024-07-17 16:36:15 +0530

    adding new file

---
+++ coalesce.sql
@@ -0,0 +1,2 @@
+#The COALESCE function in SQL is used to return the first non-NULL value from a list of expressions. 
+#It is often used to handle NULL values in queries, providing a way to substitute them with a specified default value.
