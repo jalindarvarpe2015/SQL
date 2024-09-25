41bb98f04266149784bd43924575c2bdbb7e60a5
Varpe Jalindar <jalindarvarpe2015@gmail.com>
2024-09-25 20:01:35 +0530

    addding new changes

---
+++ Union and UnionaAll.sql
@@ -0,0 +1,24 @@
+
+Union : The UNION operator is used to combine the result sets of two  or more SELECT statements into a single result set. It removes 
+dupicate rows from the result set. 
+
+UNION ALL : The UNION ALL operator, on the other hand, is used to combine the result sets of two or more SELECT statements into a single result set without removing any duplicate 
+rows.
+
+Here's an example with two tables: 
+Table 1: employees 
+
+Emp_id   Emp_name 
+1        PALLAVI 
+2        RASHI 
+3        AKSHAY 
+4        RAHUL 
+
+
+Table 2: contractors 
+Cont_id   Cont_name 
+1         HEMANT 
+2         NANDINI 
+3         PALLAVI 
+4          RAHULTable
+Cont_id 
