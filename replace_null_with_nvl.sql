964674b3df1208c15b0b22812abc5df7e45dbf51
Varpe Jalindar <jalindarvarpe2015@gmail.com>
2024-12-04 20:26:47 +0530

    adding NVL to handle with null

---
+++ replace_null_with_nvl.sql
@@ -0,0 +1,10 @@
+'''
+The NVL function in SQL is used to handle NULL values. It replaces a NULL value with a specified replacement value.
+
+NVL(expression, replacement_value)
+
+expression: The column or value to check for NULL.
+replacement_value: The value to return if expression is NULL
+
+'''
+
